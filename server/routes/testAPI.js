5999ab062b6b658e79b0659066076cb4a9f9cdd8
Rich Ewin <richardewinlambert@gmail.com>
2020-07-21 16:35:51 +0100

    api reads to the back

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
     res.send('API is working properly');
 });
 
+router.post('/', function(req, res) {
+    console.log(req.body.posted_data.mode)
+});
+
 module.exports = router;
