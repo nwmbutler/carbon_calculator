807a1a55211df0b5496a6d4932035bcf7cb0d921
Nikita <latinisnotdead07@gmail.com>
2020-07-21 12:30:12 +0100

    Changes test to find standard React message

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Carbon Calculator/i);
+  const linkElement = getByText(/Welcome to React Folks/i);
   expect(linkElement).toBeInTheDocument();
 });
