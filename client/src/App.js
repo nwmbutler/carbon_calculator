6e17d653688424040f0277d6ccb619b90f938067
Nick Butler <nwmbutler@gmail.com>
2020-07-21 12:23:49 +0100

    app.js missed from commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    callAPI() {
+        fetch("http://localhost:5000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+    componentDidMount() {
+        this.callAPI();
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React Folks</h1>
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Carbon Calculator
-        </p>
-      </header>
-    </div>
-  );
+                 <p className="App-intro">{this.state.apiResponse}</p>
+                </header>
+            </div>
+        );
+    }
 }
-
 export default App;
