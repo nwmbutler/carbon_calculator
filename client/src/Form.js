03b163f27a391438a4317e7c4ccc950659ae5c12
J Perrin <perrinjack96@gmail.com>
2020-07-21 15:09:44 +0100

    added transport mode option

--- client/src/Form.js
+++ client/src/Form.js
@@ -5,13 +5,16 @@ export default class Form extends React.Component {
     super(props);
 
     this.state = { origin: '',
-  destination: ''
+  destination: '',
+  mode: ''
   };
   }
 
 
   handleInputChange = e => {
+    alert("chenaged")
     this.setState({
+      
       [e.target.name]: e.target.value,
     });
   };
@@ -20,19 +23,14 @@ export default class Form extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
 
-    const { origin, destination } = this.state;
+    const { origin, destination, mode } = this.state;
 
     const journey = {
-      origin, destination
+      origin, destination, mode
     };
-    alert(journey.destination)
+
+    alert(journey.mode)
   }
-
-
-
-
-
-
 
 
   render() {
@@ -51,6 +49,15 @@ export default class Form extends React.Component {
             name = 'destination'
             onChange={this.handleInputChange}
           />
+          
+    <select name = 'mode' onChange={this.handleInputChange} value={this.state.mode}> >
+        <option value="null" ></option>
+        <option value="Car" >Car</option>
+        <option value="Bike" >Bike</option>
+        <option value="Walk" >Walk</option>
+       
+      </select>
+      
           <button type="submit">
                 Calculate
               </button>
