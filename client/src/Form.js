f3a7caf8c079e063e06443ce290ea35b07381b66
J Perrin <perrinjack96@gmail.com>
2020-07-21 14:36:27 +0100

    2 fieldsfrom and to fields receive params

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,18 +1,59 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { origin: '',
+  destination: ''
+  };
+  }
+
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { origin, destination } = this.state;
+
+    const journey = {
+      origin, destination
+    };
+    alert(journey.destination)
+  }
+
+
+
+
+
+
+
+
   render() {
       return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h1>Carbon Calculator</h1>
           <p>From</p>
           <input
             type="text"
+            name="origin"
+            onChange={this.handleInputChange}
           />
           <p>To</p>
           <input
             type="text"
+            name = 'destination'
+            onChange={this.handleInputChange}
           />
+          <button type="submit">
+                Calculate
+              </button>
         </form>
       );
     }
