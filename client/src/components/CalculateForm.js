bd15625b929cb3a9195d2f8d50c7854152a85366
Rich Ewin <richardewinlambert@gmail.com>
2020-07-22 10:16:14 +0100

    nodemon added

--- client/src/components/CalculateForm.js
+++ client/src/components/CalculateForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-export default class Form extends React.Component {
+export default class CalculateForm extends React.Component {
   constructor(props) {
     super(props);
 
