726b80436d0ee27887bcada35ad995372e995f14
J Perrin <perrinjack96@gmail.com>
2020-07-22 12:20:45 +0100

    returned data from google api stored in variabel

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -5,15 +5,18 @@ var axios = require ('axios');
 
 
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+  var distance = 100
+    res.send('distance');
 });
 
 router.post('/', function(req, res) {
     console.log(req.body.posted_data)
 
-  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington+DC&destinations=New+York+City,NY&key=')
+  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington+DC&destinations=New+York+City,NY&key=AIzaSyB6H02HJghnU01WhAX5rvMOqPd5l08fdiU')
   .then(function (response) {
-    console.log(response.data.rows[0].elements[0].distance.value);
+    var distance = response.data.rows[0].elements[0].distance.value
+    console.log(distance)
+
   });
 });
 
