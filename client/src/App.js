8a83de0dbc4f2a4ef3f938dbd9b1a8eda7d1057d
Nick Butler <nwmbutler@gmail.com>
2020-07-22 18:50:34 +0100

    changed api call to backend on heroku

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
         this.state = { apiResponse: "" };
     }
     callAPI() {
-        fetch("http://localhost:4567/testAPI")
+        fetch("https://carbon-backend2.herokuapp.com/testAPI")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
