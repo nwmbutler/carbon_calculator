cc3341a18ca132188a32c34b5b3bd121b27115e7
Nick Butler <nwmbutler@gmail.com>
2020-07-22 13:53:28 +0100

    tests passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Welcome to React Folks/i);
+  const linkElement = getByText(/Carbon Calculator/i);
   expect(linkElement).toBeInTheDocument();
 });
