eda539cc28b03cd1ff014733730a399f96467bff
Rich Ewin <richardewinlambert@gmail.com>
2020-07-22 19:38:24 +0100

    recommit

--- client/src/components/CalculateForm.js
+++ client/src/components/CalculateForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import "./Form.css";
 
 export default class CalculateForm extends React.Component {
   constructor(props) {
@@ -14,8 +15,8 @@ export default class CalculateForm extends React.Component {
 
 async callAPI(new_data) {
       const response = await axios.post("http://localhost:5000/testAPI", { posted_data: new_data })
-      console.log('Returned data:', response.data.answer);
-      this.setState({ apiResponse: response.data.answer })
+      // console.log('Returned data:', response.data.distance);
+      this.setState({ apiResponse: response.data.distance })
 
     } catch (e) {
       console.log(`Axios request failed: ${e}`);
@@ -40,35 +41,44 @@ async callAPI(new_data) {
     this.callAPI(journey)
   }
 
+
+
   render() {
       return (
         <form onSubmit={this.handleSubmit}>
-          <h1>{this.state.apiResponse}</h1>
-          <p>From</p>
+        <h1>Carbon Calculator</h1><br />
           <input
             type="text"
             name="origin"
+            placeholder='Choose starting point...'
             onChange={this.handleInputChange}
-          />
-          <p>To</p>
+          /><br />
+          <br />
           <input
             type="text"
             name = 'destination'
+            placeholder='Choose destination...'
             onChange={this.handleInputChange}
-          />
+          /><br />
+          <br />
 
-    <select name = 'mode' onChange={this.handleInputChange} value={this.state.mode}> >
+        <select name = 'mode' onChange={this.handleInputChange} value={this.state.mode}>
         <option value="null" ></option>
         <option value="driving" >Car</option>
         <option value="bicycling" >Bike</option>
         <option value="walking" >Walk</option>
 
-      </select>
+
+
+      </select><br /><br />
 
           <button type="submit">
                 Calculate
               </button>
-        </form>
+
+              <h3>{this.state.apiResponse} </h3>
+              </form>
+
       );
     }
   }
