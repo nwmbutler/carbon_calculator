169fbe1279a76dedd2715defa3b26e6cb28ad092
J Perrin <perrinjack96@gmail.com>
2020-07-22 12:51:39 +0100

    distance data received by react from express

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -4,18 +4,14 @@ var axios = require ('axios');
 
 
 
-router.get('/', function(req, res, next) {
-  var distance = 100
-    res.send('distance');
-});
-
 router.post('/', function(req, res) {
     console.log(req.body.posted_data)
 
-  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington+DC&destinations=New+York+City,NY&key=AIzaSyB6H02HJghnU01WhAX5rvMOqPd5l08fdiU')
+  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington+DC&destinations=New+York+City,NY&key=')
   .then(function (response) {
     var distance = response.data.rows[0].elements[0].distance.value
     console.log(distance)
+    res.send({ answer:  distance});
 
   });
 });
