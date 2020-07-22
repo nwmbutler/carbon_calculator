a95ffca4a1e485d3b02379bfd5fa3440454910b2
Rich Ewin <richardewinlambert@gmail.com>
2020-07-22 17:53:06 +0100

    refactored methods

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -2,21 +2,23 @@ var express = require('express');
 var router = express.Router();
 var axios = require ('axios');
 
-
-
 router.post('/', function(req, res) {
-    var testing = req.body.posted_data
-    console.log(testing)
+    var journey_data_out = req.body.posted_data
+    // console.log(journey_data_out)
 
-  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json',
-  {
-    params: { origins: testing.origin, destinations: testing.destination, mode: testing.mode, key: ''}})
+axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+
+  params: {
+    origins: journey_data_out.origin,
+    destinations: journey_data_out.destination,
+    mode: journey_data_out.mode,
+    key: 'AIzaSyDiJ3hGxadOyarSWI0NjyUbV2aVNUK3-kE'}})
+
   .then(function (response) {
-    console.log(response)
-    var distance = response.data.rows[0].elements[0].distance.value
-    console.log(distance)
-    res.send({ answer:  distance});
-
+    // console.log(response)
+    var google_data_in = response.data.rows[0].elements[0]
+    // console.log(distance)
+    res.send({ distance: google_data_in.distance.value});
   });
 });
 
