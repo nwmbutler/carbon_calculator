719448279698767c4cae38f928dd150bcf80ccff
J Perrin <perrinjack96@gmail.com>
2020-07-22 15:18:05 +0100

    nodemon added to ensure server reloading

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -7,7 +7,7 @@ var axios = require ('axios');
 router.post('/', function(req, res) {
     console.log(req.body.posted_data)
 
-  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=boston&destinations=New+York+City,NY&key=')
+  axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=london&destinations=cambridge+UK&key=AIzaSyC2wVcNoihfxKfL4skQA_kbht4AmywY__0')
   .then(function (response) {
     var distance = response.data.rows[0].elements[0].distance.value
     console.log(distance)
