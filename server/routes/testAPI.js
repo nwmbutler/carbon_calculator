426a28c20b5efca864ddfce32025227c10b82ad4
Rich Ewin <richardewinlambert@gmail.com>
2020-07-22 11:04:18 +0100

    axios get request

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var axios = require ('axios');
+
+
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
 });
 
 router.post('/', function(req, res) {
-    console.log(req.body.posted_data.mode)
+    console.log(req.body.posted_data)
+
+  axios.get()
+  .then(function (response) {
+    console.log(response.data.distance);
+  });
 });
 
 module.exports = router;
