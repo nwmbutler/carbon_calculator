c6e19202bf920df9a7a62bed1a29a85f92e44975
Nick Butler <nwmbutler@gmail.com>
2020-07-22 18:44:59 +0100

    backend extracted

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
         this.state = { apiResponse: "" };
     }
     callAPI() {
-        fetch("http://localhost:5000/testAPI")
+        fetch("http://localhost:4567/testAPI")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
